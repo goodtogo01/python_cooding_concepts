394b4262e3a52ee62f8e632af52ff2a68782cbff
kz1985 <khosruzzaman.ny@gmail.com>
2024-03-31 02:15:32 -0400

    topic_String questions added

---
+++ Python_Main_Fundamental_Area/topic_String.py
@@ -0,0 +1,19 @@
+# Exercise 1A: Create a string made of the first, middle and last character
+# Exercise 1B: Create a string made of the middle three characters
+# Exercise 2: Append new string in the middle of a given string
+# Exercise 3: Create a new string made of the first, middle, and last characters of each input string
+# Exercise 4: Arrange string characters such that lowercase letters should come first
+# Exercise 5: Count all letters, digits, and special symbols from a given string
+# Exercise 6: Create a mixed String using the following rules
+# Exercise 7: String characters balance Test
+# Exercise 8: Find all occurrences of a substring in a given string by ignoring the case
+# Exercise 9: Calculate the sum and average of the digits present in a string
+# Exercise 10: Write a program to count occurrences of all characters within a string
+# Exercise 11: Reverse a given string
+# Exercise 12: Find the last position of a given substring
+# Exercise 13: Split a string on hyphens
+# Exercise 14: Remove empty strings from a list of strings
+# Exercise 15: Remove special symbols / punctuation from a string
+# Exercise 16: Removal all characters from a string except integers
+# Exercise 17: Find words with both alphabets and numbers
+# Exercise 18: Replace each special symbol with # in the following string
