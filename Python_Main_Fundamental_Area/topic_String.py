0aaca57103117d257f6d68d3c5fdd20aa220fc5a
kz1985 <khosruzzaman.ny@gmail.com>
2024-03-31 22:06:57 -0400

    topic_String is working progress

--- Python_Main_Fundamental_Area/topic_String.py
+++ Python_Main_Fundamental_Area/topic_String.py
@@ -1,6 +1,40 @@
-# Exercise 1A: Create a string made of the first, middle and last character
-# Exercise 1B: Create a string made of the middle three characters
-# Exercise 2: Append new string in the middle of a given string
+# todo Exercise 1A: Create a string made of the first, middle and last character
+print('Exercise 1A: Create a string made of the first, middle and last character')
+s = 'zaman'
+print("Actual String : ", s)
+s2 = s[0::2]
+print("first, midle, and last charecters are : ",s2)
+
+# todo Exercise 1B: Create a string made of the middle three characters
+print('\nExercise 1B: Create a string made of the middle three characters')
+name = 'ABCDEABCD'
+print("Actual String : ", name)
+ln = int(len(name)/2)
+f = name[ln-1: ln+2]
+print("middle three characters : ",f)
+l =int(len(name))
+
+print('Len = ', l, '\nHalf of len = ',round(l/3, None))
+cc = name[4::2]
+print("cc " , cc)
+# res = name[s1-1:s1+2]
+print("ffll : ", name[0::2]) # ACE
+print("ffll : ", name[1::2]) # BD
+
+
+# todo Exercise 2: Append new string in the middle of a given string
+print('\nExercise 2: Append new string in the middle of a given string')
+s1 = 'khosru'
+s2 = 'zaman'
+
+m = int(len(s1)/2)
+x = s1[:m:]
+x = x + s2
+x = x + s1[m:]
+print(x)
+
+
+
 # Exercise 3: Create a new string made of the first, middle, and last characters of each input string
 # Exercise 4: Arrange string characters such that lowercase letters should come first
 # Exercise 5: Count all letters, digits, and special symbols from a given string
