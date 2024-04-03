dfac66df92f4a05e03e7aeceb90cf2265f1c9699
kz1985 <khosruzzaman.ny@gmail.com>
2024-04-03 00:17:28 -0400

    topic_String questions added

--- Python_Main_Fundamental_Area/topic_String.py
+++ Python_Main_Fundamental_Area/topic_String.py
@@ -1,45 +1,119 @@
 # todo Exercise 1A: Create a string made of the first, middle and last character
+import collections
+import json
+
 print('Exercise 1A: Create a string made of the first, middle and last character')
 s = 'zaman'
 print("Actual String : ", s)
 s2 = s[0::2]
-print("first, midle, and last charecters are : ",s2)
+print("first, midle, and last charecters are : ", s2)
 
 # todo Exercise 1B: Create a string made of the middle three characters
 print('\nExercise 1B: Create a string made of the middle three characters')
 name = 'ABCDEABCD'
 print("Actual String : ", name)
-ln = int(len(name)/2)
-f = name[ln-1: ln+2]
-print("middle three characters : ",f)
-l =int(len(name))
 
-print('Len = ', l, '\nHalf of len = ',round(l/3, None))
-cc = name[4::2]
-print("cc " , cc)
-# res = name[s1-1:s1+2]
-print("ffll : ", name[0::2]) # ACE
-print("ffll : ", name[1::2]) # BD
 
+def mdlChars(x):
+    ln = len(x) // 2
+    res = x[ln - 1:ln + 2]
+    return res
+
+
+print("Middle three characters:", mdlChars(name))
 
 # todo Exercise 2: Append new string in the middle of a given string
 print('\nExercise 2: Append new string in the middle of a given string')
-s1 = 'khosru'
-s2 = 'zaman'
-
-m = int(len(s1)/2)
-x = s1[:m:]
-x = x + s2
-x = x + s1[m:]
-print(x)
+a = 'ABCD'
+b = 'OO'
 
 
+def mixStr(x, y):
+    le = len(x) // 2
+    results = x[:le] + y + x[le:]
+    return results
 
-# Exercise 3: Create a new string made of the first, middle, and last characters of each input string
-# Exercise 4: Arrange string characters such that lowercase letters should come first
-# Exercise 5: Count all letters, digits, and special symbols from a given string
-# Exercise 6: Create a mixed String using the following rules
+
+print(f"Concatinate with {a} and {b} as :", mixStr(a, b))
+
+# todo Exercise 3: Create a new string made of the first, middle, and last characters of each input string
+print('\nExercise 3: Create a new string made of the first, middle, and last characters of each input string')
+s = 'ABCDE'
+
+
+def final_str(s):
+    res = s[0::2]
+    return res
+
+
+print(f'first, middle, and last characters of given string as {s} :', final_str(s))
+
+# todo Exercise 4: Arrange string characters such that lowercase letters should come first
+print('\nExercise 4: Arrange string characters such that lowercase letters should come first')
+s = 'abcdABCD'
+l=[]
+u=[]
+for i in s:
+    if i.islower():
+     l.append(i)
+    else:
+       u.append(i)
+res = " ".join(l+u)
+print(f"Lower is come first from {s} : ",res)
+
+def lower_and_upper_check(m):  # only lower or uppder case
+    lo = []
+    up = []
+    for i in str(m):
+        if i.islower():  # for upper case i.isupper()
+            lo.append(i)
+        else:
+            up.append(i)
+    return lo
+
+# TODO Exercise 5: Count all letters, , and special symbols from a given string
+print('\nExercise 5: Count all letters, , and special symbols from a given string')
+s = "Welcom 123@#$%> '' "
+f = collections.Counter(s)
+print("Here is total elements of counts : ",  json.dumps(f, indent=2))
+print('\nMethod 2: ')
+
+alpha =0
+digit = 0
+symbol = 0
+for i in s:
+    if i.isalpha():
+        alpha = alpha+1
+    elif i.isdigit():
+        digit = digit + 1
+    else:
+        symbol = symbol+1
+print("Alpha : ",alpha, "\nDigits : ",digit,"\nSymble : ",symbol)
+
+
+
+# todo Exercise 6: Create a mixed String using the following rules
+print('\nExercise 6: Create a mixed String using the following rules')
+a = 'ABC'
+b = 'XYZ'
+s3 = " "
+
+for i in range(len(a)):
+    s3 = s3 +a[i]
+    s3 = s3+b[-1-i]
+print("Method 1 : ",s3)
+
+def mixchars(a,b):
+    s = " "
+    for i in range(len(a)):
+        s = s+a[i]
+        s = s+b[-1-i]
+    return s
+
+print('Method 2: ',mixchars(a, b))
+
 # Exercise 7: String characters balance Test
+
 # Exercise 8: Find all occurrences of a substring in a given string by ignoring the case
 # Exercise 9: Calculate the sum and average of the digits present in a string
 # Exercise 10: Write a program to count occurrences of all characters within a string
@@ -51,3 +125,5 @@ print(x)
 # Exercise 16: Removal all characters from a string except integers
 # Exercise 17: Find words with both alphabets and numbers
 # Exercise 18: Replace each special symbol with # in the following string
+
+# 100335
--- RRRR.py
+++ RRRR.py
@@ -1,114 +1,84 @@
-# Exercise  1:  Reverse  a  list  in  Python
-import collections
+# # Exercise 1A: Create a string made of the first, middle and last character
+# def ll():
+#     m = input("Enter your string: ")
+#     c = m[0::2]
+#     return c
+# # print('Here is your string : ',ll(), end=" ")
 #
-# o = [100, 200, 300, 400, 500]
-# # m=o[::-1]
-# o.reverse()
-# # print(o)
+# # Exercise 1B: Create a string made of the middle three characters
+# def mm():
+#     m = input("Enter your string: ")
+#     l = len(m)//2
+#     c = m[l-1:l+2]
+#     return c
+# # print("he : ", mm())
+# # Exercise 2: Append new string in the middle of a given string
+# s1 = "AE"
+# s2 = "BCD"
 #
-# # Exercise  2:  Concatenate  two  lists  index-wise
-# # following condition
-# # combine / sum of index value
-# # keep same index
-# list1 = ["M", "na", "i", "Ke"]
-# list2 = ["y", "me", "s", "lly"]
-# # ['My', 'name', 'is', 'Kelly'] -
-# # i+j i+j i+j i+j single iteration
-# m = [i+j for i,j in zip(list1, list2)]
-# print(m)
+# def tc(a, b):
+#     # l = len(a)//2
+#     m = a[:1]+b+a[-1:]
+#     return m
+# # print(tc(s1, s2))
+# # Exercise 3: Create a new string made of the first, middle, and last characters of each input string
+# def to():
+#     m = input("Enter your str : ")
+#     c = m[0::2]
+#     return c
+# # print("Here you go : ", to())
+# # Exercise 4: Arrange string characters such that lowercase letters should come first
+# s = "abcdefGHIJKL"
+# l =[]
+# u=[]
+# for i in s:
+#     if i.islower():
+#         l.append(i)
+#     else:
+#         u.append(i)
+# res = "".join(l+u)
+# # print(res)
+# # Exercise 5: Count all letters, digits, and special symbols from a given string
+# m = "ABCDefgh1234!@#$.,]["
 #
-# # Exercise  4:  Concatenate  two  lists  in  the  following  order
-# l1 = ["Hello ", "take "]
-# l2 = ["Dear", "Sir"]
-# # multi iteration i[0] j[0], i[0] j[1], i[1] j[0], i[1] j[1]
-# # out put: ['Hello Dear', 'Hello Sir', 'take Dear', 'take Sir']
-# m2 = [i+j for i in l1 for j in l2]
-# print(m2)
-# # Exercise  3:  Turn  every  item  of  a  list  into  its  square
-# l = [2,4,5,6]
-# f =[]
-# #  [4, 16, 25, 36]
-# for i in l:
-#     f.append(i*i)
-# print(f)
+# dig = ""
+# alf = ""
+# num = ""
 #
-# # Exercise  5:  Iterate  both  lists  simultaneously with reverse order of list2
-# list1 = [10, 20, 30, 40]
-# list2 = [100, 200, 300, 400]
-# """
-# 10 400
-# 10 300
-# 10 200
-# 10 100
-# """
-# def ll(a, b):
-#     for i in a:
-#         for j in b[::-1]:
-#             print(i, j)
-#         break
-# ll(list1, list2)
-# # Exercise  6:  Remove  empty  strings  from  the  list  of  strings
-# li = ["Mike", "", "Emma", "Kelly", "", "Brad"]
-# print("Actual list: ", li)
-# for i in li:
-#     if i == "":
-#         li.remove(i)
-#
-# print(li)
-#
-# # Exercise  7:  Add  new  item  to  list  after  a  specified  item
-# nestedLoop = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-# print("\nExercise  7 Original list : ", nestedLoop)
-# nestedLoop.insert(3, 11)
-# print("Exercise  7 Updated list : ", nestedLoop)
-# # Exercise  8:  Extend  nested  list  by  adding  the  sublist
-# ml = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-# print("\nOriginal list : ", ml)
-# sl = [1,2,3]
-# ml[2].insert(3, sl)
-# print(ml)
-#
-# # Exercise  9:  Replace  list’s  item  with  new  value  if  found
-# # exptd  [5, 10, 15, 1111, 25, 50, 20]
-# cl = [5, 10, 15, 20, 25, 50, 20]
-# print("\nOriginal list : ", cl)
-# indx = cl.index(50)
-# cl[5]=91
-#
-#
-# print("After removing 20  : ",cl)
-#
-#
-# # Exercise  10:  Remove  all  occurrences  of  a  specific  item  from  a  list.
-# ul = [5, 10, 15, 20, 25, 50, 20]
-# print("\nOriginal list : ", ul)
-# while 20 in ul:
-#     ul.remove(20)
-# print("After removing multiple occurance : ", ul)
-#
-#
-# """
-# Exercise 9: Replace list’s item with new value if found
-# original list :  [5, 10, 15, 20, 25, 50, 20]
-# After removing 20 :  [5, 10, 15, 11, 25, 50, 20]
-# """
-l = {1,1,2,3,4}
-m = {1,1,2,5,6}
-com = l.union(m) # only common + uncommon items but no duplicate
-print("common + uncommon ", com)
-fl = l.intersection(m) # only common
-print("only common  ", fl)
-# print(l.count(1))
-tom = l.symmetric_difference(m)
-
-print("Only uncommon: ", tom)
-mol = l.difference(m)
-print("Different set1 from set2 : ", mol)
+# for i in m:
+#     if i.isdigit():
+#         dig = dig+i
+#     elif i.isalpha():
+#         alf = alf+i
+#     else:
+#         num = num+i
+# print("Dig : ", dig, "\nAlf : ",alf, "\nNum :", num)
 
 
-flc =[5, 10, 15, 20, 25, 50, 20, 20]
+# Exercise 6: Create a mixed String using the following rules
+m = "1234567@#$%^&FGHJ34%678"
+dig=""
+alphas = ""
+num = ""
+r = ""
+f = ""
+for i in m:
+    if i.isidentifier():
+        f += i
+    elif i.isdigit():
+        dig +=i
+    elif i.isnumeric():
+        num +=i
+    else:
+        r +=i
+print("Dig : ", dig, "\nAlf : ",alphas, "\nNum :", num, "\nRest of : ", r, "\nf",f)
 
-lom = []
-m = len(lom)
-lom = collections.Counter(flc)
-print(f"found {m} in the list around : ", lom , "Times")
+m = 'ABCfsldfDEdfueiuUILT'
+l =''
+u =''
+for i in m:
+    if i.islower():
+        l +=i
+    else:
+        u +=i
+print("\nLower : ", sorted(l), ",\nUpper : ",sorted(u))
