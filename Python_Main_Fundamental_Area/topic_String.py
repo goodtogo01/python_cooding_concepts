bd98a5d364ac62aace193515425a1e3287f369ce
kz1985 <khosruzzaman.ny@gmail.com>
2024-04-03 00:20:17 -0400

    topic_String questions completed until 6

--- Python_Main_Fundamental_Area/topic_String.py
+++ Python_Main_Fundamental_Area/topic_String.py
@@ -1,6 +1,8 @@
 # todo Exercise 1A: Create a string made of the first, middle and last character
 import collections
 import json
+import re
+import string
 
 print('Exercise 1A: Create a string made of the first, middle and last character')
 s = 'zaman'
@@ -51,15 +53,16 @@ print(f'first, middle, and last characters of given string as {s} :', final_str(
 # todo Exercise 4: Arrange string characters such that lowercase letters should come first
 print('\nExercise 4: Arrange string characters such that lowercase letters should come first')
 s = 'abcdABCD'
-l=[]
-u=[]
+l = []
+u = []
 for i in s:
     if i.islower():
-     l.append(i)
+        l.append(i)
     else:
-       u.append(i)
-res = " ".join(l+u)
-print(f"Lower is come first from {s} : ",res)
+        u.append(i)
+res = " ".join(l + u)
+print(f"Lower is come first from {s} : ", res)
+
 
 def lower_and_upper_check(m):  # only lower or uppder case
     lo = []
@@ -71,26 +74,25 @@ def lower_and_upper_check(m):  # only lower or uppder case
             up.append(i)
     return lo
 
+
 # TODO Exercise 5: Count all letters, , and special symbols from a given string
 print('\nExercise 5: Count all letters, , and special symbols from a given string')
 s = "Welcom 123@#$%> '' "
 f = collections.Counter(s)
-print("Here is total elements of counts : ",  json.dumps(f, indent=2))
+print("Here is total elements of counts : ", json.dumps(f, indent=2))
 print('\nMethod 2: ')
 
-alpha =0
+alpha = 0
 digit = 0
 symbol = 0
 for i in s:
     if i.isalpha():
-        alpha = alpha+1
+        alpha = alpha + 1
     elif i.isdigit():
         digit = digit + 1
     else:
-        symbol = symbol+1
-print("Alpha : ",alpha, "\nDigits : ",digit,"\nSymble : ",symbol)
-
-
+        symbol = symbol + 1
+print("Alpha : ", alpha, "\nDigits : ", digit, "\nSymble : ", symbol)
 
 # todo Exercise 6: Create a mixed String using the following rules
 print('\nExercise 6: Create a mixed String using the following rules')
@@ -99,31 +101,142 @@ b = 'XYZ'
 s3 = " "
 
 for i in range(len(a)):
-    s3 = s3 +a[i]
-    s3 = s3+b[-1-i]
-print("Method 1 : ",s3)
+    s3 = s3 + a[i]
+    s3 = s3 + b[-1 - i]
+print("Method 1 : ", s3)
 
-def mixchars(a,b):
+
+def mixchars(a, b):
     s = " "
     for i in range(len(a)):
-        s = s+a[i]
-        s = s+b[-1-i]
+        s = s + a[i]
+        s = s + b[-1 - i]
     return s
 
-print('Method 2: ',mixchars(a, b))
 
-# Exercise 7: String characters balance Test
+print('Method 2: ', mixchars(a, b))
 
-# Exercise 8: Find all occurrences of a substring in a given string by ignoring the case
-# Exercise 9: Calculate the sum and average of the digits present in a string
-# Exercise 10: Write a program to count occurrences of all characters within a string
-# Exercise 11: Reverse a given string
-# Exercise 12: Find the last position of a given substring
-# Exercise 13: Split a string on hyphens
-# Exercise 14: Remove empty strings from a list of strings
-# Exercise 15: Remove special symbols / punctuation from a string
-# Exercise 16: Removal all characters from a string except integers
-# Exercise 17: Find words with both alphabets and numbers
-# Exercise 18: Replace each special symbol with # in the following string
+# todo Exercise 7: String characters balance Test
+print('\nExercise 7: String characters balance Test')
+def bal_chk(a, b):
+    flag = True
+    for i in a:
+        if i in b:
+            continue
+        else:
+            flag = False
+    return flag
 
+# case 1:
+s1 = "Yn"
+s2 = "PYnative"
+print("Case 1: ", bal_chk(s1, s2))
+# case 2:
+s1 = "Ynf"
+s2 = "PYnative"
+print("Case 2: ", bal_chk(s1, s2))
+
+# todo Exercise 8: Find all occurrences of a substring in a given string by ignoring the case
+print('\nExercise 8: Find all occurrences of a substring in a given string by ignoring the case')
+'''Write a program to find all occurrences of “USA” in a given string ignoring the case.'''
+
+str1 = "Welcome to USA. usa awesome, isn't it?"
+sub_str="USA"
+temp_str = sub_str.lower()
+c = temp_str.count(sub_str.lower())
+print(c)
+
+# todo Exercise 9: Calculate the sum and average of the digits present in a string
+print('\nExercise 9: Calculate the sum and average of the digits present in a string')
+main_str = "PYnative29@#8496"
+dig = 0
+sum_dig =0
+for i in main_str:
+    if i.isdigit():
+        dig +=int(i)
+        sum_dig +=1
+avg = dig/sum_dig
+print("Total Digit : ",dig, "\nSum of Dig : ",sum_dig, "\nAvg : ",avg)
+
+
+# todo Exercise 10: Write a program to count occurrences of all characters within a string
+print('\nExercise 10: Write a program to count occurrences of all characters within a string')
+str1 = "Welcome to USA"
+final_string = collections.Counter(str1)
+print("Number of occurrence : ", sorted(final_string))
+
+# todo Exercise 11: Reverse a given string
+print('\nExercise 11: Reverse a given string')
+str1 = "Welcome to USA"
+rev = str1[::-1]
+print(rev)
+# todo Exercise 12: Find the last position of a given substring
+'''rfind() function used to find last indexed place of sub or given string'''
+print('\nExercise 12: Find the last position of a given substring')
+s1 = "Emma is a data scientist who knows Python. Emma works at google."
+sub_str = "Emma"
+ff = s1.rfind(sub_str)
+print(f"Last position of [{sub_str}] at : ",ff)
+
+# todo Exercise 13: Split a string on hyphens
+print('\nExercise 13: Split a string on hyphens')
+s ='Emma-is-a-data-scientist'
+sub_str = s.split("-")
+
+for i in sub_str:
+    print(i)
+# todo Exercise 14: Remove empty strings from a list of strings
+print('\nExercise 14: Remove empty strings from a list of strings')
+s = ["Emma", "Jon", "", "Kelly", None, "Eric", ""]
+print("Original String : ", s)
+final_list = []
+for i in s:
+    if i:
+        final_list.append(i)
+print("Method : 1 \nAfter removing Empty string : ", final_list)
+
+n =list(filter(None, s))
+print("Method : 2 \nAfter removing Empty string : ",n)
+
+# todo Exercise 15: Remove special symbols / punctuation from a string
+print('\nExercise 15: Remove special symbols / punctuation from a string')
+ss = "/*Jon is @developer & musician"
+print("Original string is ", ss)
+m = ss.translate(str.maketrans('', '', string.punctuation))
+print("After removing special chars : ", m)
+f = re.sub(r'[\s\w]', '', ss)
+print(f)
+# res = re.sub(r'[^\w\s]', '', str1)
+
+
+
+# todo Exercise 16: Removal all characters from a string except integers
+print('\nExercise 16: Removal all characters from a string except integers')
+str1 = 'I am 25 years and 10 months old'
+mm = ""
+for i in str1:
+    if i.isdigit():
+        mm +=i
+print("Method 1 : ",mm)
+res = "".join([i for i in str1 if i.isdigit()])
+print("Method 2 : ", res)
+
+# todo Exercise 17: Find words with both alphabets and numbers
+print('\nExercise 17: Find words with both alphabets and numbers')
+s3 = "Emma25 is Data scientist50 and AI Expert"
+r = []
+temp = s3.split()
+for i in s3.split():
+    if any(c.isalpha() for c in i) and any(c.isdigit() for c in i):
+        r.append(i)
+for j in r:
+    print(j)
+# todo Exercise 18: Replace each special symbol with # in the following string
+print('Exercise 18: Replace each special symbol with # in the following string')
+s = "Bangladesh$$@@"
+print("Before updating : ", s)
+r = "#"
+for i in string.punctuation:
+    s = s.replace(i, r)
+print("After Updating : ",s)
 # 100335
--- RRRR.py
+++ RRRR.py
@@ -1,84 +1,133 @@
-# # Exercise 1A: Create a string made of the first, middle and last character
-# def ll():
-#     m = input("Enter your string: ")
-#     c = m[0::2]
-#     return c
-# # print('Here is your string : ',ll(), end=" ")
-#
-# # Exercise 1B: Create a string made of the middle three characters
-# def mm():
-#     m = input("Enter your string: ")
-#     l = len(m)//2
-#     c = m[l-1:l+2]
-#     return c
-# # print("he : ", mm())
-# # Exercise 2: Append new string in the middle of a given string
-# s1 = "AE"
-# s2 = "BCD"
-#
-# def tc(a, b):
-#     # l = len(a)//2
-#     m = a[:1]+b+a[-1:]
-#     return m
-# # print(tc(s1, s2))
-# # Exercise 3: Create a new string made of the first, middle, and last characters of each input string
-# def to():
-#     m = input("Enter your str : ")
-#     c = m[0::2]
-#     return c
-# # print("Here you go : ", to())
-# # Exercise 4: Arrange string characters such that lowercase letters should come first
-# s = "abcdefGHIJKL"
-# l =[]
-# u=[]
-# for i in s:
-#     if i.islower():
-#         l.append(i)
-#     else:
-#         u.append(i)
-# res = "".join(l+u)
-# # print(res)
-# # Exercise 5: Count all letters, digits, and special symbols from a given string
-# m = "ABCDefgh1234!@#$.,]["
-#
-# dig = ""
-# alf = ""
-# num = ""
-#
-# for i in m:
-#     if i.isdigit():
-#         dig = dig+i
-#     elif i.isalpha():
-#         alf = alf+i
-#     else:
-#         num = num+i
-# print("Dig : ", dig, "\nAlf : ",alf, "\nNum :", num)
+import collections
+import re
 
-
+# Exercise 1A: Create a string made of the first, middle and last character
+# Exercise 1B: Create a string made of the middle three characters
+# Exercise 2: Append new string in the middle of a given string
+# Exercise 3: Create a new string made of the first, middle, and last characters of each input string
+# Exercise 4: Arrange string characters such that lowercase letters should come first
+# Exercise 5: Count all letters, digits, and special symbols from a given string
 # Exercise 6: Create a mixed String using the following rules
-m = "1234567@#$%^&FGHJ34%678"
-dig=""
-alphas = ""
-num = ""
-r = ""
-f = ""
-for i in m:
-    if i.isidentifier():
-        f += i
-    elif i.isdigit():
-        dig +=i
-    elif i.isnumeric():
-        num +=i
-    else:
-        r +=i
-print("Dig : ", dig, "\nAlf : ",alphas, "\nNum :", num, "\nRest of : ", r, "\nf",f)
+s1 = "Abc"
+s2 = "Xyz"
+s3 = " "
+for i in range(len(s1)):
+    s3 = s3 + s1[i]
+    s3 = s3 + s2[-1-i]
+print(s3)
 
-m = 'ABCfsldfDEdfueiuUILT'
-l =''
-u =''
-for i in m:
-    if i.islower():
-        l +=i
-    else:
-        u +=i
-print("\nLower : ", sorted(l), ",\nUpper : ",sorted(u))
+
+# print(s3)
+# # Exercise 7: String characters balance Test
+# def cb(a, b):
+#     flag = True
+#     for i in a:
+#         if i in b:
+#             continue
+#         else:
+#             flag = False
+#     return flag
+# x = "Py"
+# y = "Python"
+# print(cb(x, y))
+# x = "Pyf"
+# y = "Python"
+# print(cb(x, y))
+#
+#
+# # Exercise 8: Find all occurrences of a substring in a given string by ignoring the case
+# str1 = "Emma is a data scientist and emma EmmA, eMmA knows Python. Emma works at google."
+# gs = "Emma"
+# f = str1.lower().count(gs.lower())
+# print(f"Total number of occurrence of {gs} is : ",f," Times.")
+# # Exercise 9: Calculate the sum and average of the digits present in a string
+# str1 = "PYnative29@#8496"
+# dig = 0
+# sum_dig = 0
+#
+# for i in str1:
+#     if i.isdigit():
+#         dig += int(i)
+#         sum_dig +=1
+#
+# avg  = dig / sum_dig
+# print("\nDigits = ", dig, "\nSum = ", sum_dig, "\nAvg = ", avg)
+# # Exercise 10: Write a program to count occurrences of all characters within a string
+# s = "Apple"
+# f = collections.Counter(s)
+# print("\nOccurrance : ", f)
+# # Exercise 11: Reverse a given string
+# s = "Apple"
+# print("\n", s[::-1])
+#
+#
+# def ll(a):
+#     m = "".join(reversed(a))
+#     print(m)
+#
+#
+# ll(s)
+# # Exercise 12: Find the last position of a given substring
+# """Write a program to find the last position of a substring “Emma” in a given string."""
+# str1 = "Emma is a data scientist who knows Python. Emma works at google."
+# gs = "Emma"
+# mm = str1.rfind(gs)
+# cc = str1.find(gs)
+# print("\nUsing find() method : ", cc)
+# print(f"Index position of {gs} at : ", mm)
+# # Exercise 13: Split a string on hyphens
+# str1 = 'Emma-is-a-data-scientist'
+# print("\nOriginal String with HyPhens : ", str1)
+# res = re.sub(r'[^\w\s]', '  ', str1)
+# print("Method 1 : ", res)
+# print("Method 2 : ")
+# sp = str1.split("-")
+# for i in sp:
+#     print(i)
+# # Exercise 14: Remove empty strings from a list of strings
+#
+# o = ['Emma', 'Jon', '', 'Kelly', None, 'Eric', '']
+# print('\nOriginal list of sting : ', o)
+# t = list(filter(None, o))
+# print("After removing : ", t)
+# # Exercise 15: Remove special symbols / punctuation from a string
+#
+# str1 = "/*Jon is @developer & musician"
+# print("\nOriginal string is : ", str1)
+# res = re.sub(r'[^\w\s]', '', str1)
+# print(res)
+#
+# # Exercise 16: Removal all characters from a string except integers
+# s = "Mono 1 2 is a good man 2 1"
+# tem = s.split()
+# a = ""
+# n = ""
+# r = "".join(i for i in s if i.isdigit())
+# print('\n', r)
+# for i in tem:
+#     if i.isalpha():
+#         a += i
+#     else:
+#         n += i
+#
+# print(n)
+#
+# # Exercise 17: Find words with both alphabets and numbers
+# s = "Mono12 is a good man21"
+# re = []
+# temp = s.split()
+#
+# for item in temp:
+#     if any(n.isalpha() for n in item) and any(n.isdigit() for n in item):
+#         re.append(item)
+#
+# for i in re:
+#     print(i)
+#
+# # Exercise 18: Replace each special symbol with # in the following string
+# import string
+#
+# mm = "ABCDEFGH2345*&^%$"
+# for i in string.punctuation:
+#     mm = mm.replace(i, "#")
+# print(mm)
